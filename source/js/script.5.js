d97d50c02f4a73a50f186e0b092a2ad3595da408
Ben Falk <falk.ben@gmail.com>
2018-08-20 16:41:53 -0400

    even more compact tools page

--- source/js/script.5.js
+++ source/js/script.5.js
@@ -153,7 +153,7 @@ $(document).ready(function () {
 	for (i = 0; i < coll.length; i++) {
 		coll[i].addEventListener("click", function () {
 			this.classList.toggle("active");
-			var content = this.parentNode.nextElementSibling;
+			var content = this.nextElementSibling;
 			if (content.style.maxHeight) {
 				content.style.maxHeight = null;
 			} else {
