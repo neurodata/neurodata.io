db17bd9f4700957020c7aa67022c7e19a6ab5588
Ben Falk <falk.ben@gmail.com>
2019-01-23 23:22:53 -0500

    update style on tools items (#169)
    * update style on tools items
    
    * minor fixes for pos. sticky
    
    * adds tooltip with package names for each tool item
    
    * fix duplicated css for data-detail page on tool items
    
    * add slight rounded edges to each tool grouping
    
    * fix bottom right radius not having effect on expanded tool

--- source/js/script.8.js
+++ source/js/script.8.js
@@ -157,7 +157,6 @@ $(document).ready(function () {
 
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
-
 	for (i = 0; i < coll.length; i++) {
 		coll[i].addEventListener("click", function () {
 			this.classList.toggle("active");
