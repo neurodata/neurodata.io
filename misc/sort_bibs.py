80a64b00f83d27ae3d72c9e3a6bfa1c2f33c1c50
Ben Falk <falk.ben@gmail.com>
2019-12-13 13:33:48 -0500

    Sort bibs (#345)
    * sort the bib entries by key/id
    
    * add script that sorts the bib files
    
    * merge the bib files.
    
    * revert to state before merging bib files.
    
    * merge bib files
    
    * fix comments.
    
    * fix CoRR
    
    * caught some more curly brackets.
    
    * remove curly bracket.:

---
+++ misc/sort_bibs.py
@@ -0,0 +1,33 @@
+"""Sort the bib files by ID (for doing a diff)
+"""
+
+
+import codecs
+
+import bibtexparser
+
+from .create_bibs import entries_to_file
+
+
+bibs = [
+    (
+        "content/research/publications.html",
+        "content/pubs/pubs.bib",
+        ["pre_prints", "peer_reviewed", "conf", "tech_reports", "other"],
+        "pubs",
+    ),
+    (
+        "content/research/talks.html",
+        "content/talks/talks.bib",
+        ["invited", "other"],
+        "talks",
+    ),
+]
+
+for bib in bibs:
+    with codecs.open(bib[1], "r", "utf-8") as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
+    entries = bib_database.entries
+    entries_sort = sorted(entries, key=lambda x: x["ID"])
+
+    entries_to_file(entries_sort, bib[1])
